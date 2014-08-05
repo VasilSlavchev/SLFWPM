3dfdf353cf81952f1f54b79a876b0c8c831c5943
Vasil Slavchev <vasilslavchev@gmail.com>
2014-08-05 18:08:03 +0300

    READ_ME.html :)

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * SLFWPM - Simple Login Form With PM Messages.
+ *
+ * This is config file for Simple PM Message by Vaseto^^ 2014.
+ *
+ * More at: (VasilSLavchev.net or vasilslavchev@gmail.com).
+ *
+ * https://bitbucket.org/VasilSlavchev/simpleloginformwithpm
+ *
+ * This project is under creative commons license: CC BY-NC-SA.
+ *
+ * https://creativecommons.org/licenses/by-nc-sa/2.5/bg/
+ *
+ * https://creativecommons.org/licenses/by-nc-sa/2.5/bg/deed.en
+ *
+*/
+
+
+
+
+
+
+
+
+
+
+
+ ?>
